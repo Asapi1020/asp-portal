90e71050116d345f95b2aa203d9664ab80320bf8
Asapi1020 <88693257+Asapi1020@users.noreply.github.com>
2026-03-27 15:52:24 +0900

    ページ遷移でトップへスクロールするように変更

--- src/frameworks/react/App.tsx
+++ src/frameworks/react/App.tsx
@@ -1,20 +1,34 @@
-import type { JSX } from "react";
-import { Route, Routes } from "react-router-dom";
+import { type JSX, useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { RootLayout } from "./layouts/RootLayout";
 import { About, Game, Home, NewsList, NotFound } from "./pages";
 import { NewsDetails } from "./pages/news/NewsDetails";
 
 export const App = (): JSX.Element => {
 	return (
-		<Routes>
-			<Route path="/" element={<RootLayout />}>
-				<Route index element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/news" element={<NewsList />} />
-				<Route path="/news/:id" element={<NewsDetails />} />
-				<Route path="/game" element={<Game />} />
-				<Route path="*" element={<NotFound />} />
-			</Route>
-		</Routes>
+		<>
+			<ScrollToTop />
+			<Routes>
+				<Route path="/" element={<RootLayout />}>
+					<Route index element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/news" element={<NewsList />} />
+					<Route path="/news/:id" element={<NewsDetails />} />
+					<Route path="/game" element={<Game />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
+			</Routes>
+		</>
 	);
+};
+
+const ScrollToTop = (): null => {
+	const { pathname } = useLocation();
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: 不明なエラー
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+	return null;
 };
