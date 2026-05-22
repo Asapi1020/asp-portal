3641c740116f3c0e79a4270633c620626a848706
Asapi1020 <88693257+Asapi1020@users.noreply.github.com>
2026-05-23 07:41:45 +0900

    change base directory to build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	base: "./",
+	base: "/",
 	plugins: [react(), svgr()],
 	resolve: {
 		alias: {
